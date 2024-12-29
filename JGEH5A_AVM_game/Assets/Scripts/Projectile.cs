05bed9babf5fbfed88185337fba915d84098ccba
Ju974 <julienganne974@gmail.com>
2024-12-30 00:50:17 +0100

    add vfx + visual for the projectile

--- JGEH5A_AVM_game/Assets/Scripts/Projectile.cs
+++ JGEH5A_AVM_game/Assets/Scripts/Projectile.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Projectile : MonoBehaviour
 {
 
     private Rigidbody bulletRigidBody;
     [SerializeField] private GameObject player;
-
+    [SerializeField] private GameObject explositionEffect;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -21,10 +22,22 @@ public class Projectile : MonoBehaviour
         bulletRigidBody.velocity = transform.forward * speed;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator ProjectileVFXManager(GameObject VFX)
     {
-        if(other.gameObject != player)// prevent the player and his ball from colliding
-        Destroy(gameObject);
+        GameObject NewVFX = Instantiate(VFX, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Destroying VFX");
+        Destroy(NewVFX, 1.0f);
+    }
+    private void OnTriggerEnter(Collider other)
+    { // prevent the player and his ball from colliding
+        if (other.gameObject != player)
+        {
+            GameObject NewVFX = Instantiate(explositionEffect, transform.position, Quaternion.identity);
+           // StartCoroutine(ProjectileVFXManager(explositionEffect));
+            Destroy(NewVFX, 1.0f);
+            Destroy(gameObject);
+        }
     }
     // Update is called once per frame
     void Update()
