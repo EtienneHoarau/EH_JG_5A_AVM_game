c7a9f030b9a97378dacb76c4a3083cc61e655008
EtienneHoarau <76174089+EtienneHoarau@users.noreply.github.com>
2025-01-29 02:52:55 +0100

    Pause menu v1
    Menu Pause fonctionnel mais moche

---
+++ JGEH5A_AVM_game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    public void Paused()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale=0;
+        gameIsPaused=true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale=1;
+        gameIsPaused=false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
