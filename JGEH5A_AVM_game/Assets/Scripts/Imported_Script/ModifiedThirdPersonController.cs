a2614ea43234ef6203756a1e9d12ceed2b00ccb4
Ju974 <julienganne974@gmail.com>
2025-02-02 01:25:34 +0100

    g-oublié-de-save-1-truc
    désolé

--- JGEH5A_AVM_game/Assets/Scripts/Imported_Script/ModifiedThirdPersonController.cs
+++ JGEH5A_AVM_game/Assets/Scripts/Imported_Script/ModifiedThirdPersonController.cs
@@ -164,6 +164,7 @@ namespace StarterAssets
 #else
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
+            gameManager = GameManager.Instance;
 
             AssignAnimationIDs();
 
